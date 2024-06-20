ca95b6db69fb525f534d52ab905031a01c543e9e
Joanne Yeung <jttyeung@gmail.com>
2024-06-20 14:49:45 -0600

    Add alt text

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,7 +2,7 @@ function ProfileCard({ title, handle, image }) {
 
   return (
     <div>
-      <img src={image} />
+      <img src={image} alt="pda logo" />
       <div>Title is {title}</div>
       <div>Handle is {handle}</div>
     </div>
